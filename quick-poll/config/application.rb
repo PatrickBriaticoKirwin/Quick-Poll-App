350349e9ecea1b683885b8075858323bec670e35
PatrickBurgerKing <patoy@live.com>
2025-03-13 10:53:48 -0400

    Use rails/all to fix build problems

--- quick-poll/config/application.rb
+++ quick-poll/config/application.rb
@@ -1,11 +1,7 @@
 require_relative "boot"
 
-require "action_controller/railtie"
-require "action_view/railtie"
-require "active_storage/engine"
-
+require "rails/all"
 #require "sprockets/railtie" # For assets
-require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
